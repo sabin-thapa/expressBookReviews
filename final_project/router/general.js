e5e11c34923428b44fdbd2dbf70930ae6d82446f
Sabin Thapa <sabint017@gmail.com>
2023-05-04 16:19:41 +0545

    feat: add Task 1 and Task 2 solutions

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -11,15 +11,19 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
+public_users.get('/',async function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const results = await SON.stringify(books)
+  return res.status(300).json(results);
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
+public_users.get('/isbn/:isbn',async function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  const filteredBook = books[isbn]
+  console.log(filteredBook, "FIltered Boo");
+  return res.status(300).json({book: filteredBook});
  });
   
 // Get book details based on author
